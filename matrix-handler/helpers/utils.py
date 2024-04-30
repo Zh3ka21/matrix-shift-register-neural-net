f80ceead3a74e346840f07e4a636120a5391461d
Євгеній Пузанов <Yevhenii.Puzanov@cs.khpi.edu.ua>
2024-04-30 16:21:42 +0300

    export matrices error

---
+++ matrix-handler/helpers/utils.py
@@ -0,0 +1,40 @@
+
+def val(degreeA, first_digitA, degreeB, first_digitB):
+    denominator = gcd(power(degreeA), power(degreeB))
+    if denominator != 1:
+        return True
+
+    numerator = power(degreeA)
+    denominator = gcd(numerator, first_digitA)
+    result = numerator / denominator
+    if result != numerator:
+        return True
+
+    numerator = power(degreeB)
+    denominator = gcd(numerator, first_digitB)
+    result = numerator / denominator
+    if result != numerator:
+        return True
+
+    return degreeA > degreeB
+
+def power(n):
+    return 2 ** n - 1
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+# def visualization(acf, r):
+#     plt.figure(figsize=(14, 6))
+#     plt.plot(range(len(acf)), acf, linestyle='-')
+#     plt.title(f'Function {r - 1 }')
+#     plt.grid(True)
+
+#     # Додамо більш детальну шкалу з боку графіка
+#     plt.minorticks_on()
+#     plt.grid(which='major', linestyle='-', linewidth='0.5', color='black')
+#     plt.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+
+#     plt.show()
